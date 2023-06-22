dc7a660a1ff46b61ddce70d06fb856b5b46fb92c
Aadarsh <aadarshbandhuaryal@gmail.com>
2023-06-22 12:00:06 +0545

    base files set-up

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas=document.querySelector("#canvas")
+const c =canvas.getContext("2d")
+const canvasW=1020
+const canvasH=600
+canvas.width=canvasW
+canvas.height=canvasH
+c.fillStyle="white"
+c.fillRect(0,0,canvasW,canvasH)
