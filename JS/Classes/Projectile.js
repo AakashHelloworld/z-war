e87db6ffbbe2516f066b0d79bf999d25c4c93a14
AakashHelloworldgit config --global user.name AakashHelloworldgit config --global user.name AakashHelloworld <aakashbandhuaryal@gmail.com>
2023-06-24 12:49:06 +0545

    Player added

---
+++ JS/Classes/Projectile.js
@@ -0,0 +1,25 @@
+class Projectile {
+    constructor(x, y, direction) {
+      this.position = { x: x, y: y };
+      this.direction = direction;
+      this.speed = 5;
+    }
+  
+    update() {
+      if (this.direction === 'left') {
+        this.position.x -= this.speed;
+      } else if (this.direction === 'right') {
+        this.position.x += this.speed;
+      } else if (this.direction === 'up') {
+        this.position.y -= this.speed;
+      }
+    }
+  
+    draw() {
+      c.fillStyle = 'black';
+      c.beginPath();
+      c.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2);
+      c.fill();
+    }
+
+  }
--- index.js
+++ index.js
@@ -7,26 +7,53 @@ const canvasH=document.querySelector(".main").getBoundingClientRect().height
 canvas.width=canvasW
 canvas.height=canvasH
 
+const key = {
+    ArrowLeft:{
+        pressed: false
+    },
+    ArrowRight:{
+        pressed: false
+    },
+    ArrowUp:{
+        pressed: false
+    }
+}
+
 const capsuleArray=[]
 let counter=1
 const buffer=3
 const enemy=new ENEMYSPACESHIP()
-
+const player = new Player();
 const animate=()=>{
     window.requestAnimationFrame(animate)
-    if(buffer%counter==0){
+
+    // if(buffer%counter==0){
         c.clearRect(0,0,canvas.width,canvas.height)
         c.fillStyle="white"
         c.fillRect(0,0,canvas.width,canvas.height)
         enemy.draw()
         enemy.update()
         // capsules iteration
+
         capsuleArray.forEach(capsule=>{
             capsule.draw()
             capsule.update()
         })
-        counter=1
+        // counter=1
+    // }
+    player.draw()
+
+    player.update()
+    if(key.ArrowLeft.pressed){
+        player.velocity.x =-5
+    }else if(key.ArrowRight.pressed){
+        player.velocity.x =5
+    }else if(!key.ArrowLeft.pressed){
+        player.velocity.x =0
     }
-    counter+=1
+    player.projectiles.forEach(projectile => {
+        projectile.update();
+        projectile.draw();
+      });
 }
 animate()
